b8b3e9c796774f4d919dfee08b6d9bc4c836dc5a
hedutrack <hedutrack@gmail.com>
2015-12-15 18:45:41 -0500

    For transport
    Settings preference WIP.

--- app/src/main/java/com/hayseed/mymovieapp/SettingsActivity.java
+++ app/src/main/java/com/hayseed/mymovieapp/SettingsActivity.java
@@ -1,12 +1,14 @@
 package com.hayseed.mymovieapp;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 /**
  *
  */
-public class SettingsActivity extends Activity
+public class SettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener
 {
     @Override
     protected void onCreate (Bundle savedInstanceState)
@@ -16,5 +18,20 @@ public class SettingsActivity extends Activity
         getFragmentManager ().beginTransaction ()
                 .replace (android.R.id.content, new SettingsFragment ())
                 .commit ();
+
+        bindPreferenceSummaryToValue (findPreference (getString (R.string.pref_location_key)));
+
     }
+
+    @Override
+    public boolean onPreferenceChange (Preference preference, Object value)
+    {
+        String stringValue = value.toString ();
+
+        preference.setSummary (stringValue);
+
+        return true;
+    }
+
+
 }
--- app/src/main/java/com/hayseed/mymovieapp/SettingsFragment.java
+++ app/src/main/java/com/hayseed/mymovieapp/SettingsFragment.java
@@ -1,6 +1,7 @@
 package com.hayseed.mymovieapp;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 
@@ -20,5 +21,33 @@ public class SettingsFragment extends PreferenceFragment
 
 
         addPreferencesFromResource (R.xml.preferences);
+    }
+
+    @Override
+    public boolean onPreferenceChange (Preference preference, Object value)
+    {
+        String stringValue = value.toString ();
+
+        preference.setSummary (stringValue);
+
+        return true;
+    }
+
+    /**
+     * Attaches a listener so the summary is always updated with the preference value.
+     * Also fires the listener once, to initialize the summary (so it shows up before the value
+     * is changed.)
+     */
+    private void bindPreferenceSummaryToValue (Preference preference)
+    {
+        // Set the listener to watch for value changes.
+        preference.setOnPreferenceChangeListener (this);
+
+        // Trigger the listener immediately with the preference's
+        // current value.
+        onPreferenceChange (preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences (preference.getContext ())
+                        .getString (preference.getKey (), ""));
     }
 }
